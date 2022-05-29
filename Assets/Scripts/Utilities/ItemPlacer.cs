6f7ac706fb4f355c425caeee33dcdb0bcd71fa88
Eren Erdoğan <dactarium@outlook.com>
2022-05-29 17:19:48 +0300

    Item Placer

---
+++ Assets/Scripts/Utilities/ItemPlacer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPlacer : MonoBehaviour
+{
+    [Header("Itemler Sahnede Olmalı (Görünmeyen Bir yerde)")]
+    [SerializeField] private Transform[] _items;
+
+    [Header("SpawnPointler Sahnede olmalı")]
+    [SerializeField] private List<Transform> _spawnPoints;
+
+    private void Start()
+    {
+        foreach (Transform item in _items)
+        {
+            if (_spawnPoints.Count <= 0) return;
+
+            Transform spawn = _spawnPoints[Random.Range(0, _spawnPoints.Count)];
+            item.position = spawn.transform.position;
+
+            _spawnPoints.Remove(spawn);
+        }
+    }
+}
